8fdb70bdafdbee0c31f251ae6f2634bbfce9d383
Jacob Dymond <thedevjake@gmail.com>
2019-02-14 08:27:30 -0600

    Initial

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const url = '../docs/pdf.pdf'
+
+let pdfDoc = null,
+pageNum = 1,
+pageIsRendering = false,
+pageNumIsPending = null;
+
+const scale = 1.5,
+canvas = document.querySelector('#pdf-render'),
+ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+
+}
+
+
+// Get the Document
+pdfjsLib.getDocument(url).promise.then(_pdfDoc => {
+pdfDoc = _pdfDoc;
+
+document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+renderPage(pageNum)
+
+
+})
+
